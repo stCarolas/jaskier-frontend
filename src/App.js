bf9d43ca7fc4823f33d1e3bbf3fe33ea296825be
stCarolas <stcarolas@gmail.com>
2023-03-23 19:37:00 +0300

    draft:

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+	function Song() {
+		const [songs, setSongs] = useState([]);
+
+		useEffect(() => {
+			fetch('http://localhost:8080/events')
+				.then(data => data.json())
+				.then((json) => { setSongs( json ); });
+		}, []);
+
+		if (songs.length > 0) {
+			return (<><div>Song: {songs[0].result.results[0].artist}</div></>);
+		}
+		return <div>Loading...</div>;
+	}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+				<Song />
       </header>
     </div>
   );
